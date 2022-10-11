32c79a2b50c2283d3211d47283c96fb91ee140c2
nrasel <nrasel452@gmail.com>
2022-10-12 02:13:51 +0600

    add model and instance

--- app.js
+++ app.js
@@ -8,51 +8,120 @@ app.use(express.json());
 app.use(cors());
 
 // schema design
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name of this product"],
-    unique: [true, "Name must be unique"],
-    minLength: [3, "Name must be at least 3 characters"],
-    maxLength: [100, "Name is too large"],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, "Price can be negative"],
-  },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      value: ["kg", "litre", "pcs"],
-      message: "unit value can't be {VALUE}, must be kg/litre/pcs",
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name of this product"],
+      unique: [true, "Name must be unique"],
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [100, "Name is too large"],
     },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "quantity can be negative"],
-    validate: {
-      validator: (value) => {
-        const isInteger = Number.isInteger(value);
-        if (isInteger) {
-          return true;
-        } else {
-          false;
-        }
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price can be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "unit value can't be {VALUE}, must be kg/litre/pcs",
       },
-      message: "Quantity must be an integer",
     },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "quantity can be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            false;
+          }
+        },
+        message: "Quantity must be an integer",
+      },
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "Status can't be {VALUE}",
+      },
+    },
+    // supplier: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Supplier",
+    // },
+    // categories: [
+    //   {
+    //     name: {
+    //       type: String,
+    //       required: true,
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId,
+    //   },
+    // ],
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+//  Schema -> model -> query
+const Product = mongoose.model("Product", productSchema);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
+});
+// mongoose middlewares for saving data : pre/post
+productSchema.pre("save", function (next) {
+  console.log("Before saving data");
+  if (this.quantity === 0) {
+    this.status = "out-of-stock";
+  }
+  next();
+});
+
+// posting to database
+
+app.post("/api/v1/product", async (req, res, next) => {
+  // save or create
+  try {
+    // create method
+    // const result = await Product.create(req.body);
+
+    // save method
+    const product = new Product(req.body);
+
+    // instance creation -> Do something -> save()
+    if (product.quantity === 0) {
+      product.status = "out-of-stock";
+    }
+
+    const result = await product.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "Data inserted successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Data inserted failed",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
