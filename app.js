053edc3c52f2cf74ed8f8de64a8d17b3524ab901
nrasel <nrasel452@gmail.com>
2022-10-11 21:50:28 +0600

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-
+// middlewares
 app.use(express.json());
 app.use(cors());
-
-
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
 
 module.exports = app;
